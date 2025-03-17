9945d60c434f4c6769eabb99641a05e9101626d5
zsuper <piyushkumbhare10@gmail.com>
2025-03-16 21:10:21 -0700

    Added cycling workspaces per monitor and added Prime Video to Firefox's opacity-ignore

---
+++ scripts/cycle-monitor-ws.sh
@@ -0,0 +1,29 @@
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 [prev|next]"
+    exit 1
+fi
+
+if [ "$1" = "next" ]; then
+    op="+"
+elif [ "$1" = "prev" ]; then
+    op="-"
+else
+    echo "Invalid argument: $1"
+    exit 1
+fi
+        
+    
+
+active=$(hyprctl activewindow -j)
+if [ "$active" = "Invalid" ];
+then
+    hyprctl dispatch workspace m+1
+    exit 1
+fi
+
+active_monitor=$(echo $active | jq '.monitor')
+active_workspace=$(echo $active | jq '.workspace.id')
+
+next_workspace=$(hyprctl workspaces -j | jq "[.[] | select(.monitorID == $active_monitor and .id > 0)] | sort_by(.id) | map(.id) | .[(index($active_workspace) $op 1) % length]")
+
+hyprctl dispatch workspace $next_workspace
