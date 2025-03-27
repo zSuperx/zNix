eb6023f60831e27d2eb7bcd75488176436aed563
zsuper <piyushkumbhare10@gmail.com>
2025-03-27 01:20:46 -0700

    testing to see if username change worked, idk why im doing it this way...

--- system/home.nix
+++ system/home.nix
@@ -17,23 +17,24 @@
     ./modules/terminal.nix # Terminal config
     ./modules/nvf.nix # Neovim config
   ];
+  home = {
+    username = "zsuper";
+    homeDirectory = "/home/zsuper";
 
-  home.username = "zsuper";
-  home.homeDirectory = "/home/zsuper";
+    # Manages dotfiles by symlinking at build time
+    file = {
+      ".config/nvim/init.lua".source = ../nvim/init.lua;
+      ".config/nvim/lua".source = ../nvim/lua;
+      ".config/hypr".source = ../hypr;
+      ".config/wofi".source = ../wofi;
+      ".config/yazi".source = ../yazi;
+    };
 
-  # Manages dotfiles by symlinking at build time
-  home.file = {
-    ".config/nvim/init.lua".source = ../nvim/init.lua;
-    ".config/nvim/lua".source = ../nvim/lua;
-    ".config/hypr".source = ../hypr;
-    ".config/wofi".source = ../wofi;
-    ".config/yazi".source = ../yazi;
+    sessionVariables = {
+      EDITOR = "nvim";
+    };
+    stateVersion = "24.11";
   };
 
-  home.sessionVariables = {
-    EDITOR = "nvim";
-  };
-
-  programs.home-manager.enable = true;
-  home.stateVersion = "24.11"; # Don't change this probably
+  programs.home-manager.enable = true; # Don't change this probably
 }
