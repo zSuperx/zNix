7de06d5e42a43fb34bd346f410f36f84947634e9
zsuper <piyushkumbhare10@gmail.com>
2025-07-07 19:29:00 -0700

    tmux, niri, and assets updates

--- modules/dev/tmux/tmux.nix
+++ modules/dev/tmux/tmux.nix
@@ -12,14 +12,16 @@
         extraConfig = ''
           bind -T copy-mode-vi v send -X begin-selection
           bind -T copy-mode-vi y send-keys -X copy-pipe "pbcopy"
-          bind -T copy-mode-vi Escape send-keys -X clear-selection
           bind -T copy-mode-vi i send-keys -X cancel
           bind -T copy-mode-vi a send-keys -X cancel
         '';
 
         plugins = with pkgs.tmuxPlugins; [
           tmux-which-key
+          tmux-sessionx
+          tmux-fzf
           nord
+          sensible
         ];
       };
     };
