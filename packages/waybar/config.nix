cfd56edccfcaf8695de888d41239bcac91e8da2f
zsuper <piyushkumbhare10@gmail.com>
2026-01-05 22:22:32 -0800

    waybar styling changes (remove `[]`)

--- packages/waybar/config.nix
+++ packages/waybar/config.nix
@@ -33,40 +33,27 @@ in
     "custom/power"
   ];
   backlight = {
-    format = "  {icon} {percent}%";
-    format-icons = [
-      "[ ░░░░░░░░░░ ]"
-      "[ ▓░░░░░░░░░ ]"
-      "[ ▓▓░░░░░░░░ ]"
-      "[ ▓▓▓░░░░░░░ ]"
-      "[ ▓▓▓▓░░░░░░ ]"
-      "[ ▓▓▓▓▓░░░░░ ]"
-      "[ ▓▓▓▓▓▓░░░░ ]"
-      "[ ▓▓▓▓▓▓▓░░░ ]"
-      "[ ▓▓▓▓▓▓▓▓░░ ]"
-      "[ ▓▓▓▓▓▓▓▓▓░ ]"
-      "[ ▓▓▓▓▓▓▓▓▓▓ ]"
-    ];
+    format = "   {percent}%";
     scroll-step = 5;
   };
   battery = {
     format = " {icon} {capacity}%";
     format-alt = " {time} {icon}";
     format-charging = " {icon} {capacity}%󱐋";
-    format-critical = "  [ ░░░░░░░░░░ ] {capacity}%";
+    format-critical = "  {capacity}%";
     format-full = " {icon} {capacity}%";
     format-icons = [
-      " [ ░░░░░░░░░░ ]"
-      "󰁺 [ ▓░░░░░░░░░ ]"
-      "󰁻 [ ▓▓░░░░░░░░ ]"
-      "󰁼 [ ▓▓▓░░░░░░░ ]"
-      "󰁽 [ ▓▓▓▓░░░░░░ ]"
-      "󰁾 [ ▓▓▓▓▓░░░░░ ]"
-      "󰁿 [ ▓▓▓▓▓▓░░░░ ]"
-      "󰂀 [ ▓▓▓▓▓▓▓░░░ ]"
-      "󰂁 [ ▓▓▓▓▓▓▓▓░░ ]"
-      "󰂂 [ ▓▓▓▓▓▓▓▓▓░ ]"
-      "󰁹 [ ▓▓▓▓▓▓▓▓▓▓ ]"
+      ""
+      "󰁺"
+      "󰁻"
+      "󰁼"
+      "󰁽"
+      "󰁾"
+      "󰁿"
+      "󰂀"
+      "󰂁"
+      "󰂂"
+      "󰁹"
     ];
     format-plugged = " {icon} {capacity}%󱐋";
     format-warning = " {icon} {capacity}%";
@@ -77,18 +64,18 @@ in
     };
   };
   bluetooth = {
-    format = "[ <span font_size=\"13pt\"> 󰂯 </span>  ]";
-    format-connected = "[  <span font_size=\"13pt\"> 󰂱 </span>  ]";
-    format-off = "[  <span font_size =\"13pt\"> 󰂲 </span>  ]";
-    format-on = "[  <span font_size=\"13pt\"> 󰂯 </span>  ]";
+    format = "󰂯";
+    format-connected = "󰂱";
+    format-off = "󰂲";
+    format-on = "󰂯";
     on-click = "${toggle-bluetooth}";
     on-click-right = "${blueman-manager}";
     tooltip-format = "{controller_alias}\t{controller_address}\n{device_enumerate}";
   };
   clock = {
-    format = "[ <span font_desc = \"Maple Mono Bold 11.2 \">{:%R %p}</span> ]";
-    format-alt = "[ <span font_desc =\"Maple Mono Bold 11.2 \">{:%m/%d/%Y}</span> ]";
-    tooltip-format = "<big>{:%d %A }</big>\n<tt><span font_desc=\"Maple Mono Bold \">{calendar}</span></tt>";
+    format = "<span font_desc = \"JetbrainsMono Nerd Font Bold \">{:%R %p}</span>";
+    format-alt = "<span font_desc = \"JetbrainsMono Nerd Font Bold \">{:%m/%d/%Y}</span>";
+    tooltip-format = "<big>{:%d %A }</big>\n<tt><span font_desc=\"JetbrainsMono Nerd Font Bold \">{calendar}</span></tt>";
     on-click-right = "date +%m/%d/%Y | ${wl-copy}";
   };
   "custom/spotify" = {
@@ -102,14 +89,14 @@ in
     on-scroll-down = "playerctl -p spotify_player,spotify previous";
   };
   "custom/visual-refresh" = {
-    format = " [ <span font_size=\"13pt\"> 󰑐 </span>  ]  ";
+    format = "󰑐";
     on-click = "pkill -SIGUSR2 waybar";
     tooltip = true;
     tooltip-format = "Click to visually refresh waybar";
   };
   "custom/notifications" = {
     tooltip = true;
-    format = "[ {icon} ]";
+    format = "{icon}";
     format-icons = {
       notification = "󱅫";
       none = "󰂜";
@@ -128,7 +115,7 @@ in
     escape = true;
   };
   "custom/power" = {
-    format = "[  ⏻   ] ";
+    format = " ⏻";
     on-click = "${lib.getExe pkgs.wofi-power-menu}";
     tooltip = false;
   };
@@ -136,23 +123,23 @@ in
   layer = "top";
   position = "bottom";
   network = {
-    format-disabled = " [    󰤭     ]";
-    format-disconnected = " [    󰤩     ]";
-    format-ethernet = " [    모    ]";
+    format-disabled = "󰤭";
+    format-disconnected = "󰤩";
+    format-ethernet = "모";
     format-icons = [
-      "󰤟 "
-      "󰤢 "
-      "󰤥 "
-      "󰤨 "
+      "󰤟"
+      "󰤢"
+      "󰤥"
+      "󰤨"
     ];
-    format-linked = "[ {ifname} (No IP) 모 ] ";
-    format-wifi = " [    {icon}    ]";
+    format-linked = "{ifname} (No IP) 모";
+    format-wifi = "{icon}";
     on-click = "${toggle-wifi}";
     on-click-right = "${nmgui}";
     tooltip-format = "{icon} {essid}";
   };
   power-profiles-daemon = {
-    format = "{icon} ";
+    format = "{icon}";
     format-icons = {
       balanced = "";
       default = "";
@@ -163,27 +150,27 @@ in
     tooltip-format = "Power profile: {profile}\nDriver: {driver}";
   };
   pulseaudio = {
-    format = " {icon} {volume}% ";
-    format-bluetooth = " {icon} {volume}%";
-    format-bluetooth-critical = " 󰖁 [ ░░░░░░░░░░ ] {volume}% ";
-    format-bluetooth-muted = " 󰖁 [ ░░░░░░░░░░ ] {volume}% ";
-    format-critical = "  󰖁 [ ░░░░░░░░░░ ] {volume}% ";
+    format = "{icon}  {volume}%";
+    format-bluetooth = "{icon}  {volume}%";
+    format-bluetooth-critical = "󰖁  {volume}%";
+    format-bluetooth-muted = "󰖁  {volume}%";
+    format-critical = "󰖁  {volume}%";
     format-icons = {
       default = [
-        " [ ░░░░░░░░░░ ]"
-        " [ ▓░░░░░░░░░ ]"
-        " [ ▓▓░░░░░░░░ ]"
-        " [ ▓▓▓░░░░░░░ ]"
-        " [ ▓▓▓▓░░░░░░ ]"
-        " [ ▓▓▓▓▓░░░░░ ]"
-        " [ ▓▓▓▓▓▓░░░░ ]"
-        " [ ▓▓▓▓▓▓▓░░░ ]"
-        " [ ▓▓▓▓▓▓▓▓░░ ]"
-        " [ ▓▓▓▓▓▓▓▓▓░ ]"
-        " [ ▓▓▓▓▓▓▓▓▓▓ ]"
+        ""
+        ""
+        ""
+        ""
+        ""
+        ""
+        ""
+        ""
+        ""
+        ""
+        ""
       ];
     };
-    format-muted = " 󰖁 [ ░░░░░░░░░░ ] {volume}% ";
+    format-muted = "󰖁 {volume}%";
     on-click = "${wpctl} set-mute @DEFAULT_AUDIO_SINK@ toggle";
     scroll-step = 5;
     states = {
@@ -191,5 +178,4 @@ in
       warning = 5;
     };
   };
-  spacing = 4;
 }
