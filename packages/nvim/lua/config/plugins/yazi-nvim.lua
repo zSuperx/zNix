a3f5ba8752a1d1bbc722b298a61b4417ed3a360d
zsuper <piyushkumbhare10@gmail.com>
2026-02-11 19:38:49 -0800

    yazi-nvim pdf opener
    configured yazi-nvim to run `xdg-open $@` if opening a PDF

--- packages/nvim/lua/config/plugins/yazi-nvim.lua
+++ packages/nvim/lua/config/plugins/yazi-nvim.lua
@@ -1,4 +1,16 @@
-require("yazi").setup()
+require("yazi").setup({
+  --- @param chosen_file string
+  open_file_function = function(chosen_file, config, state)
+    local pdf_suffix = ".pdf"
+    if chosen_file:sub(- #pdf_suffix):lower() == pdf_suffix then
+      -- Open PDFs with xdg-open and detach the process
+      vim.fn.jobstart({ "xdg-open", chosen_file }, { detach = true })
+    else
+      -- Open everything else normally
+      vim.cmd(string.format("edit %s", vim.fn.fnameescape(chosen_file)))
+    end
+  end,
+})
 
-vim.keymap.set("n", "<leader>-", ":Yazi<CR>", { desc = "Add new file in project" })
-vim.keymap.set("n", "<leader>cw", ":Yazi cwd<CR>", { desc = "Add new file in project" })
+vim.keymap.set("n", "<leader>-", ":Yazi<CR>", { desc = "Open Yazi in current file's directory" })
+vim.keymap.set("n", "<leader>cw", ":Yazi cwd<CR>", { desc = "Open Yazi in project root" })
